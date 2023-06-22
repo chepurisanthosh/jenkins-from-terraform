809ef8ce59c7e4932cc45d67052c4cbcbeb16e29
chepurisanthosh <52729976+chepurisanthosh@users.noreply.github.com>
2023-06-22 16:41:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -98,7 +98,7 @@ ingress {
 
 # 8. Create elastic_ip
 resource "aws_eip" "one" {
-   vpc                       = true
+   domain                    = "vpc"
    network_interface         = aws_network_interface.web-server-nic.id
    associate_with_private_ip = "10.0.1.50"
    depends_on                = [aws_internet_gateway.gw]
